f8fe48f9737ab6c7572f50e49e77afa42df1733e
MarioPC <sqlmeister2@gmail.com>
2024-10-26 13:22:56 +0200

    Pierwszy commit, zainicjowanie repozytorium

---
+++ main.py
@@ -0,0 +1,12 @@
+#wypisać jakie są typy danych w python
+
+
+#castowanie wartości
+x = 10.2
+print(x)
+x = int(x)
+print(x)
+
+# # Pozwala na zwrócienie wartości logicznej czy wartośc jest danej klasy/typu
+# print(isinstance('x', str))
+
